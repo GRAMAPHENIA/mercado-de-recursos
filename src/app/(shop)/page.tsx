fbbe1300396d9fcf4cf5d62d04ba2b6a29916bf3
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-11 15:00:57 -0300

    nav-work

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,7 +3,7 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <h1>HOME</h1>
       <h1 className={`${titleFont.className} font-semibold`}>App</h1>
     </div>
