47e60917bdc972f52c85b220bb94bc7b433cfda4
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-12 12:10:30 -0300

    menu-lateral-segunda-parte

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -1,6 +1,16 @@
 "use client";
 
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5"; // Asegúrate de que el nombre del icono es correcto
+import {
+  IoCloseOutline,
+  IoSearchOutline,
+  IoPersonOutline,
+  IoTicketOutline,
+  IoLogInOutline,
+  IoLogOutOutline,
+  IoShirtOutline,
+  IoPeopleOutline,
+} from "react-icons/io5";
+import Link from "next/link";
 
 export const SideBar = () => {
   return (
@@ -19,7 +29,7 @@ export const SideBar = () => {
         <IoCloseOutline
           size={50}
           className="absolute top-5 right-5 cursor-pointer"
-          onClick={() => console.log("Clicked")} // Asegúrate de tener una función definida
+          onClick={() => console.log("Clicked")}
         />
 
         {/* Input */}
@@ -32,6 +42,66 @@ export const SideBar = () => {
             className="w-full bg-stone-50 rounded pl-10 py-1 pr-1 border-b-2 text-xl border-stone-200 focus: outline-none focus:border-slate-400"
           />
         </div>
+
+        {/* Menu */}
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoPersonOutline size={30} />
+          <span className="ml-3 text-xl">Perfil</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoLogInOutline size={30} />
+          <span className="ml-3 text-xl">Ingresar</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoLogOutOutline size={30} />
+          <span className="ml-3 text-xl">Salir</span>
+        </Link>
+
+        {/* Line Separator */}
+        <div className="w-full h-px bg-stone-200 my-10" />
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoShirtOutline size={30} />
+          <span className="ml-3 text-xl">Productos</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-stone-100 rounded transition-all"
+        >
+          <IoPeopleOutline size={30} />
+          <span className="ml-3 text-xl">Usuarios</span>
+        </Link>
       </nav>
     </div>
   );
