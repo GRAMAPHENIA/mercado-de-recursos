244b4cb0d30cef4e2229022cc8f74c2279b3d463
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-12 09:32:52 -0300

    grid-de-productos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,15 @@
-import { titleFont } from "@/config/fonts";
 import { Title } from "@/components/title/Title";
+import { initialData } from "@/seed/seed";
+import { ProdcutGrid } from "@/components/products/product-grid/ProdcutGrid";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+
+      <ProdcutGrid products={products} />
     </>
   );
 }
